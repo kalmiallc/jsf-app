5cd1f01a9c56b93021a05d7d789113640a0f8bf5
Matej Gorjanc <matej.gorjanc@kalmia.si>
2020-12-17 15:48:26 +0100

    Badge fixes.

--- src/lib/kal-jsf-doc/layout-library/custom-components/badge/jsf-badge.component.ts
+++ src/lib/kal-jsf-doc/layout-library/custom-components/badge/jsf-badge.component.ts
@@ -9,6 +9,7 @@ import Color                                                                    
 })
 export class JsfBadgeComponent implements OnInit {
 
+  private _isInitialized = false;
 
   @Input() title: string;
 
@@ -19,7 +20,6 @@ export class JsfBadgeComponent implements OnInit {
   public get color(): string {
     return this._color;
   }
-
   public set color(value: string) {
     this._color = value;
     this.updateColor();
@@ -43,14 +43,28 @@ export class JsfBadgeComponent implements OnInit {
   constructor(private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this._isInitialized = true;
+
     this.updateColor();
   }
 
   private updateColor() {
-    this._textColor = this.color && Color(this.color).rgb().string();
-    this._bgColor   = this.backgroundColor ? Color(this.backgroundColor).rgb().string() : (
-      this.color && Color(this.color).alpha(.2).rgb().string()
-    );
+    if (!this._isInitialized) {
+      return;
+    }
+
+    if (this.color === 'primary' || this.color === 'accent' || this.color === 'warn') {
+      this._textColor = this.color;
+    } else {
+      this._textColor = this.color && Color(this.color).rgb().string();
+    }
+
+    const bgColor = this.backgroundColor || this.color;
+    if (bgColor === 'primary' || bgColor === 'accent' || bgColor === 'warn') {
+      this._bgColor = bgColor;
+    } else {
+      this._bgColor   =  Color(bgColor).alpha(.2).rgb().string();
+    }
 
     this.cdRef.detectChanges();
   }
