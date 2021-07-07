4ea461d74bf5d8c22e3141dd79defe93ba1b51f7
Matej Gorjanc <matej.gorjanc@kalmia.si>
2021-07-07 09:13:27 +0200

    Generic filter select.

--- src/lib/jsf-filter-components.module.ts
+++ src/lib/jsf-filter-components.module.ts
@@ -3,20 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
 import { SharedModule } from './kal-jsf-doc/shared/shared.module';
 
-import { FilterPropStringComponent }            from './kal-jsf-filter/prop-filter-library/string.component';
-import { FilterPropArrayComponent }             from './kal-jsf-filter/prop-filter-library/array.component';
-import { FilterPropNumberComponent }            from './kal-jsf-filter/prop-filter-library/number.component';
-import { FilterPropBinaryComponent }            from './kal-jsf-filter/prop-filter-library/binary.component';
-import { FilterPropBooleanComponent }           from './kal-jsf-filter/prop-filter-library/boolean.component';
-import { FilterPropDateComponent }              from './kal-jsf-filter/prop-filter-library/date.component';
-import { FilterPropIdComponent }                from './kal-jsf-filter/prop-filter-library/id.component';
-import { FilterPropIntegerComponent }           from './kal-jsf-filter/prop-filter-library/integer.component';
-import { FilterPropObjectComponent }            from './kal-jsf-filter/prop-filter-library/object.component';
-import { FilterPropRefComponent }               from './kal-jsf-filter/prop-filter-library/ref.component';
-import { FilterPropTableComponent }             from './kal-jsf-filter/prop-filter-library/table.component';
-import { FilterHandlerCommonDropdownComponent } from './kal-jsf-filter/handler-filter-library/common/dropdown.component';
-import { FilterHandlerCommonRadioComponent }    from './kal-jsf-filter/handler-filter-library/common/radio.component';
+import { FilterPropStringComponent }                from './kal-jsf-filter/prop-filter-library/string.component';
+import { FilterPropArrayComponent }                 from './kal-jsf-filter/prop-filter-library/array.component';
+import { FilterPropNumberComponent }                from './kal-jsf-filter/prop-filter-library/number.component';
+import { FilterPropBinaryComponent }                from './kal-jsf-filter/prop-filter-library/binary.component';
+import { FilterPropBooleanComponent }               from './kal-jsf-filter/prop-filter-library/boolean.component';
+import { FilterPropDateComponent }                  from './kal-jsf-filter/prop-filter-library/date.component';
+import { FilterPropIdComponent }                    from './kal-jsf-filter/prop-filter-library/id.component';
+import { FilterPropIntegerComponent }               from './kal-jsf-filter/prop-filter-library/integer.component';
+import { FilterPropObjectComponent }                from './kal-jsf-filter/prop-filter-library/object.component';
+import { FilterPropRefComponent }                   from './kal-jsf-filter/prop-filter-library/ref.component';
+import { FilterPropTableComponent }                 from './kal-jsf-filter/prop-filter-library/table.component';
+import { FilterHandlerCommonDropdownComponent }     from './kal-jsf-filter/handler-filter-library/common/dropdown.component';
+import { FilterHandlerCommonRadioComponent }        from './kal-jsf-filter/handler-filter-library/common/radio.component';
 import { FilterHandlerCommonButtonToggleComponent } from './kal-jsf-filter/handler-filter-library/common/button-toggle.component';
+import { FilterGenericSelectComponent }             from '@kalmia/jsf-app/lib/kal-jsf-filter/handler-filter-library/generic-select.component';
 
 @NgModule({
   imports     : [
@@ -39,9 +40,10 @@ import { FilterHandlerCommonButtonToggleComponent } from './kal-jsf-filter/handl
 
     FilterHandlerCommonDropdownComponent,
     FilterHandlerCommonButtonToggleComponent,
-    FilterHandlerCommonRadioComponent
+    FilterHandlerCommonRadioComponent,
+    FilterGenericSelectComponent
   ],
-  exports     : [
+  exports: [
     FilterPropStringComponent,
     FilterPropNumberComponent,
     FilterPropArrayComponent,
@@ -56,7 +58,8 @@ import { FilterHandlerCommonButtonToggleComponent } from './kal-jsf-filter/handl
 
     FilterHandlerCommonDropdownComponent,
     FilterHandlerCommonButtonToggleComponent,
-    FilterHandlerCommonRadioComponent
+    FilterHandlerCommonRadioComponent,
+    FilterGenericSelectComponent
   ]
 })
 export class JsfFilterComponentsModule {}
